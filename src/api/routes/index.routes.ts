a302ff99708cd8ece571396649a2c995acc9f312
William Krisley <will.ks2101@gmail.com>
2023-02-11 10:08:26 -0300

    index routes base

---
+++ src/api/routes/index.routes.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import loginController from "../controllers/login.controller";
+import auth from "../middlewares/auth";
+import validateBody from "../middlewares/validateBody";
+import { UserLoginSchema } from "../schemas/users.schemas";
+import productRouter from "./product.routes";
+import userRouter from "./user.routes";
+
+const router = Router();
+
+router.post("/login", validateBody(UserLoginSchema), loginController);
+
+router.use(auth);
+
+router.use(userRouter);
+router.use(productRouter);
+
+export default router;
