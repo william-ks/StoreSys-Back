997df43fce717cadc6793e090948a19b69667819
William Krisley <will.ks2101@gmail.com>
2023-02-11 10:02:21 -0300

    API configs to start a server

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import router from "./api/routes/index.routes";
+import "dotenv/config";
+
+class App {
+  public express: express.Application;
+
+  public constructor() {
+    this.express = express();
+
+    this.middlewares();
+    this.routes();
+  }
+
+  private middlewares(): void {
+    this.express.use(express.json());
+  }
+
+  private routes(): void {
+    this.express.use(router);
+  }
+}
+
+export default new App().express;
