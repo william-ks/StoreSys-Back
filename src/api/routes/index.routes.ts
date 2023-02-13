0be8ac93a478d3c7d09be3b4e8935e2a2c333b0d
William Krisley <will.ks2101@gmail.com>
2023-02-13 20:58:05 -0300

    add ohter routes

--- src/api/routes/index.routes.ts
+++ src/api/routes/index.routes.ts
@@ -4,7 +4,9 @@ import auth from "../middlewares/auth";
 import validateBody from "../middlewares/validateBody";
 import { UserLoginSchema } from "../schemas/users.schemas";
 import categoryRouter from "./category.routes";
+import machinesRouter from "./machines.routes";
 import productRouter from "./product.routes";
+import saleRouter from "./sale.routes";
 import userRouter from "./user.routes";
 
 const router = Router();
@@ -16,5 +18,7 @@ router.use(auth);
 router.use(userRouter);
 router.use(productRouter);
 router.use(categoryRouter);
+router.use(machinesRouter);
+router.use(saleRouter);
 
 export default router;
