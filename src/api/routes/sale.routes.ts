147c73e72e31f4124ed056f2ba57d0d299bfec7e
William Krisley <will.ks2101@gmail.com>
2023-02-13 20:56:37 -0300

    criação das rotas de vendas

---
+++ src/api/routes/sale.routes.ts
@@ -0,0 +1,31 @@
+import { Router } from "express";
+import saleController from "../controllers/sale.controller";
+import adminAuth from "../middlewares/adminAuth";
+import validateBody from "../middlewares/validateBody";
+import {
+  machineCreateSchema,
+  machineUpdateSchema,
+} from "../schemas/machines.schema";
+
+const saleRouter = Router();
+
+saleRouter.get("/sales", saleController.readAll);
+saleRouter.get("/sales/:id", saleController.readOne);
+
+saleRouter.post(
+  "/sales",
+  // validateBody(machineCreateSchema),
+  saleController.create
+);
+
+saleRouter.use(adminAuth);
+
+saleRouter.put(
+  "/sales/:id",
+  // validateBody(machineUpdateSchema),
+  saleController.update
+);
+
+saleRouter.delete("/sales/:id", saleController.delete);
+
+export default saleRouter;
