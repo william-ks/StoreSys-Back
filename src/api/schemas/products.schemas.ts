8b0c4c57cb57f603bdb6d90828c4e0195d393f24
William Krisley <will.ks2101@gmail.com>
2023-02-13 21:01:46 -0300

    editing 'nullable' in zod validations

--- src/api/schemas/products.schemas.ts
+++ src/api/schemas/products.schemas.ts
@@ -5,15 +5,15 @@ export const ProductCreateSchema = z.object({
   value: z.number().min(0),
   stock: z.number().min(0),
   url: z.string().url().nullable(),
-  path: z.string(),
+  path: z.string().nullable(),
   category_id: z.number().min(0),
 });
 
 export const ProductUpdateSchema = z.object({
-  name: z.string().min(3).nullable(),
-  value: z.number().min(0).nullable(),
-  stock: z.number().min(0).nullable(),
-  url: z.string().url().nullable(),
-  path: z.string().nullable(),
-  category_id: z.number().min(0).nullable(),
+  name: z.nullable(z.string().min(3)),
+  value: z.nullable(z.number().min(0)),
+  stock: z.nullable(z.number().min(0)),
+  url: z.nullable(z.string().url()),
+  path: z.nullable(z.string()),
+  category_id: z.nullable(z.number().min(0)),
 });
