c370e2398158eaea8c7b76431901bf18a9429718
william krisley <will.ks2101@gmail.com>
2023-07-11 14:42:50 -0300

    ajustes no user schema adicionando uma validação para o codigo do produto

--- src/api/schemas/users.schemas.ts
+++ src/api/schemas/users.schemas.ts
@@ -9,6 +9,7 @@ export const UserCreateSchema = z.object({
   name: z.string().min(3),
   email: z.string().email(),
   password: z.string().min(3),
+  hierarchy_id: z.number().min(1).max(4)
 });
 
 export const UserUpdateSelfSchema = z.object({
